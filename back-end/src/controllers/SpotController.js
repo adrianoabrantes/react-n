90972898cfd5e70bcd33b811bbc4794faa425cbc
Adriano <adrianoa@disoft.com.br>
2019-10-02 18:25:31 -0300

    primeiro commit

---
+++ back-end/src/controllers/SpotController.js
@@ -0,0 +1,9 @@
+module.exports = {
+    async store(req, res) {
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+        //console.log(req.body);
+        //console.log(req.file);
+        return res.json({ ok: true })
+    }
+};
